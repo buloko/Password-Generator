11e0eb20f2e95f4a1b28e8d47a712224aca81a87
buloko <buloko8@gmail.com>
2022-10-31 23:50:17 -0400

    COMMIT TO PASS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 var password = document.getElementById("password");
 var chars = "01234567890abcdefghijklmnopqrustuvwxyz!#$%^&*()ABCDE"
 var passwordLength = 10;
