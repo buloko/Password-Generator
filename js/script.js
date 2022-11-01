2ad59763b4048d56ec63078135387a95a07fb9ad
buloko <buloko8@gmail.com>
2022-10-31 23:24:03 -0400

    Commit to PASS

--- js/script.js
+++ js/script.js
@@ -5,4 +5,5 @@ var password = "";
 for (var i = 0; i<= passwordLength; i++) {
   var randomNumber = Math.floor(Math.random()* chars.length); 
   password += chars.substring(randomNumber, randomNumber +1);
+  
 }
