79884831457618ed290f8b11b3c7250630f01a34
buloko <buloko8@gmail.com>
2023-01-29 19:30:20 -0500

    getting rid of old code

--- assets/script.js
+++ assets/script.js
@@ -5,45 +5,45 @@ var anyNum = "0123456789";
 var specialChars = "!@#$%^&*()";
 
 
-// Add event listener to generate button
-function generatePassword() {
-  var password = "";
-  var pasArray = "";
+// // Add event listener to generate button
+// function generatePassword() {
+//   var password = "";
+//   var pasArray = "";
 
-var numChar = window.prompt("How many characthers do you want to have?");
-if (numChar === null) {
-  return;
-}
+// var numChar = window.prompt("How many characthers do you want to have?");
+// if (numChar === null) {
+//   return;
+// }
 
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber + 1);
-  }
-  return password;
-}
-generateBtn.addEventListener("click", writePassword);
-
-
-if(generateBtn){
-  generateBtn.addEventListener("click", writePassword);
-}
-if(passwordText){
-  passwordText.value = password;
-}
+//   for (var i = 0; i <= passwordLength; i++) {
+//     var randomNumber = Math.floor(Math.random() * chars.length);
+//     password += chars.substring(randomNumber, randomNumber + 1);
+//   }
+//   return password;
+// }
+// generateBtn.addEventListener("click", writePassword);
 
 
-
-
-var generateBtn = document.querySelector("#generate");
+// if(generateBtn){
+//   generateBtn.addEventListener("click", writePassword);
+// }
+// if(passwordText){
+//   passwordText.value = password;
+// }
 
 
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// var generateBtn = document.querySelector("#generate");
 
-  passwordText.value = password;
 
-}
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
