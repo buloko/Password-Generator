79b7490f66add289c4e17878a9ff585c3083efa8
buloko <buloko8@gmail.com>
2023-01-29 19:32:47 -0500

    commit got rid of commented out code, added spe ch

--- assets/script.js
+++ assets/script.js
@@ -2,48 +2,4 @@
 var upperCase = "ABCDEFHJKLMNIOPQRUSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var anyNum = "0123456789";
-var specialChars = "!@#$%^&*()";
-
-
-// // Add event listener to generate button
-// function generatePassword() {
-//   var password = "";
-//   var pasArray = "";
-
-// var numChar = window.prompt("How many characthers do you want to have?");
-// if (numChar === null) {
-//   return;
-// }
-
-//   for (var i = 0; i <= passwordLength; i++) {
-//     var randomNumber = Math.floor(Math.random() * chars.length);
-//     password += chars.substring(randomNumber, randomNumber + 1);
-//   }
-//   return password;
-// }
-// generateBtn.addEventListener("click", writePassword);
-
-
-// if(generateBtn){
-//   generateBtn.addEventListener("click", writePassword);
-// }
-// if(passwordText){
-//   passwordText.value = password;
-// }
-
-
-
-
-// var generateBtn = document.querySelector("#generate");
-
-
-
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
+var specialChars = "!@#$%^&*(),.;:_-=+{}|/`";
